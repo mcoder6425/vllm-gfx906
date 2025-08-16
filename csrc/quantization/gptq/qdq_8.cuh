f5bead8ad91ac960a5ff0458391e2d661164d2cc
NaLan ZeYu <nalanzeyu@gmail.com>
2025-08-16 15:53:34 +0000

    [gfx906] optimize exllama 8bit dequant

--- csrc/quantization/gptq/qdq_8.cuh
+++ csrc/quantization/gptq/qdq_8.cuh
@@ -16,12 +16,22 @@ __forceinline__ __device__ void dequant_8bit_8(const uint32_t q_0,
                                                const uint32_t q_1,
                                                half2 (&dq)[4], int stride,
                                                const uint32_t zero) {
-  half dqh[8];
-  for (int i = 0; i < 4; i++) dqh[i] = dq_ns(exb(q_0, i * 8, 0xff), zero);
-  for (int i = 0; i < 4; i++) dqh[i + 4] = dq_ns(exb(q_1, i * 8, 0xff), zero);
+  constexpr uint32_t ext10 = 0x07010700;
+  constexpr uint32_t ext32 = 0x07030702;
+  constexpr uint32_t c0 = 0x64006400;
 
-  for (int i = 0; i < 4; i++)
-    dq[i] = __halves2half2(dqh[i * 2], dqh[i * 2 + 1]);
+  half2 z2 = __hadd2(*reinterpret_cast<const half2*>(&c0),
+                     __half2half2(__int2half_rn(zero)));
+
+  half2_uint32 qa(__builtin_amdgcn_perm(c0, q_0, ext10));
+  half2_uint32 qb(__builtin_amdgcn_perm(c0, q_0, ext32));
+  half2_uint32 qc(__builtin_amdgcn_perm(c0, q_1, ext10));
+  half2_uint32 qd(__builtin_amdgcn_perm(c0, q_1, ext32));
+
+  dq[0] = __hsub2(qa.as_half2, z2);
+  dq[1] = __hsub2(qb.as_half2, z2);
+  dq[2] = __hsub2(qc.as_half2, z2);
+  dq[3] = __hsub2(qd.as_half2, z2);
 }
 
 }  // namespace gptq
